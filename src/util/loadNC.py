15afa471631a1c22cbde055bd1d7401eaef042af
drosero <darwin11rv@gmail.com>
2018-03-05 16:01:41 -0500

    Creacion

--- src/util/loadNC.py
+++ src/util/loadNC.py
@@ -5,12 +5,10 @@
 
 descarga de datos modo experto
 
- SOURCES .UCSB .CHIRPS .v2p0 .monthly .global .precipitation
-  X (81.5W) (74E) RANGEEDGES
-  T (Jan 1981) (Jan 2017) RANGEEDGES
+  SOURCES .UCSB .CHIRPS .v2p0 .monthly .global .precipitation
+  X (82W) (74W) RANGEEDGES
   Y (3N) (6S) RANGEEDGES
-  X (81.5W) (74w) RANGEEDGES
-
+  T (Jan 1981) (Feb 2018) RANGEEDGES
 link de descarga
 
 https://iridl.ldeo.columbia.edu/SOURCES/.UCSB/.CHIRPS/.v2p0/.monthly/.global/.precipitation/X/%2881.5W%29%2874E%29RANGEEDGES/T/%28Jan%201981%29%28Jan%202017%29RANGEEDGES/Y/%283N%29%286S%29RANGEEDGES/X/%2881.5W%29%2874w%29RANGEEDGES/data.nc
@@ -20,6 +18,9 @@ https://iridl.ldeo.columbia.edu/SOURCES/.UCSB/.CHIRPS/.v2p0/.monthly/.global/.pr
 import numpy as np
 import netCDF4 as nc
 import pandas as pd
+from django.template.defaulttags import verbatim
+
+
 class LoadNC():
     """"""
 
@@ -28,22 +29,64 @@ class LoadNC():
     def readNc(self):
 
         dataset=nc.Dataset("/home/drosero/Descargas/rrchirps.nc")
-        print("leyendo el netcdf\n imprimiendo el metadato")
-        print(dataset.file_format)
-        print(dataset.dimensions.keys())
-        print(dataset.dimensions['T'])
-        print(dataset.variables.keys())
-        print(dataset.variables['precipitation'])
+        #print("leyendo el netcdf\n imprimiendo el metadato")
+        #print(dataset.file_format)
+        #print(dataset.dimensions.keys())
+        #print(dataset.dimensions['T'])
+        #print(dataset.variables.keys())
+        #print(dataset.variables['precipitation'])
         #print(dataset.variables)
         """longitud = X, latitud = Y Precipitacion = precipitation"""
+        #dataset.
         lat = dataset.variables['Y'][:]
         lon = dataset.variables['X'][:]
-        rr = dataset.variables['precipitation'][:,:,:]
-        print(rr)
+        rr = dataset.variables['precipitation'][:]
+        rr = rr.filled(np.nan)
+        rr_units = dataset.variables['precipitation'].units
+        #print("lat : ",len(lat),"lon: ",len(lon))
+        #print("units" ,rr_units)
+        print("rr length ",len(rr))
+
+        ##time serie
+        fechas = pd.date_range(start="1981-01-01",periods=len(dataset.variables['T']),freq="MS")
+        print(type(fechas),"  ",type(rr))
+
+        self.getDataAsfile(rr, lat, lon)
+        dataset.close();
 
 
-    def findLatLong(self):
+    def findCoor(self):
+        """Obtiene las cordenadas dentro del dataset NetCDF que sean las mas cooerentes """
         pass
+
+    def getDataAsfile(self, varible, lat, lon):
+
+        for t in range(0,5):
+
+            print("t : ",t," ",lat[0], " {", 0, ":", 74, "} ", lon[74], " : ", varible[t, 0, 74])
+        sLat=[]
+        sLon=[]
+        v1=[]
+        v2=[]
+        v3=[]
+        v4=[]
+        v5 = []
+        to=0
+        for i in range(0,len(lat)):
+            for j in range(0, len(lon)):
+                #print(lat[i], " {", i, ":", j, "} ", lon[j], " : ", varible[0, 0, 74])
+                sLat.append(lat[i])
+                sLon.append(lon[j])
+                v1.append(varible[0, i, j])
+                v2.append(varible[1, i, j])
+                v3.append(varible[2, i, j])
+                v4.append(varible[3, i, j])
+                v5.append(varible[4, i, j])
+        print(len(sLat),len(sLon))
+        #data={"lon":sLon,"lat":sLat,"1981-01":v1,"1981-02":v2,"1981-03":v3,"1981-04":v4,"1981-05":v5}
+        dataF=pd.DataFrame({"lon":sLon,"lat":sLat,"1981-01":v1,"1981-02":v2,"1981-03":v3,"1981-04":v4,"1981-05":v5})
+        dataF.to_csv("/home/drosero/Escritorio/chirps.csv",sep=";")
+        ##datos=pd.DataFrame(sLat,sLon,v1,v2,v3,v4,v5)
 
 
 lnc= LoadNC()
